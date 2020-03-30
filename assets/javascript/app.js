0bdc3f277b02ff5887eac9828fce1c5004a5848b
Robert Marks <rmarks@nav.com>
2020-03-30 12:09:01 -0600

    wireframe started

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var trivia = {
+
+    // Variables
+
+    clockRunning : false,
+
+    // intervalId : setInterval(count, 1000),
+
+    time : 30,
+
+    questions : [
+        {
+            question : "What is the best color?",
+            correct : "Green",
+            multipleChoice : ["Red", "Yellow", "Orange"]
+        },
+        {
+            question : "What is the best color?",
+            correct : "Green",
+            multipleChoice : ["Red", "Yellow", "Orange"]           
+        },
+        {
+            question : "What is the best color?",
+            correct : "Green",
+            multipleChoice : ["Red", "Yellow", "Orange"]
+        }
+    ]
+
+    // Methods
+
+    // start : loads and resets questions
+
+    // buildQuestion
+
+    // 
+
+
+}
+
+// 
